3779b253dc9a31524bfe9f6000ea5a37cb938129
Daniel Rebolledo <danielrebolledo.182@gmail.com>
2022-06-06 01:30:21 -0400

    Primer Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Vista Home</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+  components: {},
+}
+</script>
+<style>
+body {
+  text-align: center;
+}
+</style>
